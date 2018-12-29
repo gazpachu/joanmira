c94e96dcc1540150cf1ae0b69c3225e8121fe66a
Joan Mira <JOAN.MIRA@VODAFONE.COM>
2018-12-29 20:08:19 +0100

    Fixed top nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,144 +1,127 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import { ScreenWidthContext, FontLoadedContext } from "../../layouts";
 import config from "../../../content/meta/config";
 import Menu from "../Menu";
 import Logo from "../../images/svg/logo.svg";
 import Cursor from "../../images/gif/cursor.gif";
 
-class Header extends Component {
-  getHeaderSize = () => {
-    const homepage = this.props.path === "/" ? "homepage" : "";
+const Header = props => {
+  const { theme, path } = props;
 
-    return `${homepage}`;
-  };
+  return (
+    <Fragment>
+      <header className={`header ${path === "/" ? "homepage" : ""}`}>
+        <div className="header-content">
+          <Link to="/" className="logo">
+            <Logo className="logo-symbol" />
+            <h1>{config.headerTitle}</h1>
+            <img src={Cursor} className="cursor" />
+          </Link>
+          <Menu theme={theme} />
+        </div>
+      </header>
 
-  render() {
-    const { pages, path, theme } = this.props;
+      <style jsx>{`
+        .header {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          width: 100%;
+          padding: 0 20px;
+          text-align: center;
 
-    return (
-      <Fragment>
-        <header className={`header ${this.getHeaderSize()}`}>
-          <div className="header-content">
-            <Link to="/" className="logo">
-              <Logo className="logo-symbol" />
-              <h1>{config.headerTitle}</h1>
-              <img src={Cursor} className="cursor" />
-            </Link>
-            <FontLoadedContext.Consumer>
-              {loaded => (
-                <ScreenWidthContext.Consumer>
-                  {width => (
-                    <Menu
-                      path={path}
-                      screenWidth={width}
-                      fontLoaded={loaded}
-                      pages={pages}
-                      theme={theme}
-                    />
-                  )}
-                </ScreenWidthContext.Consumer>
-              )}
-            </FontLoadedContext.Consumer>
-          </div>
-        </header>
+          @below tablet {
+            padding-top: 16px;
+          }
 
-        {/* --- STYLES --- */}
-        <style jsx>{`
-          .header {
+          &:after {
+            content: "";
             position: absolute;
             top: 0;
             left: 0;
-            right: 0;
             width: 100%;
-            padding: 36px 20px 0 20px;
-            text-align: center;
+            height: 150px;
+            background: linear-gradient(to bottom, #fff 0, rgba(255, 255, 255, 0) 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ffffff', GradientType=0 );
 
             @below tablet {
-              padding-top: 16px;
+              height: 250px;
+            }
+          }
+
+          .header-content {
+            z-index: 1;
+            position: relative;
+            max-width: 979px;
+            margin: 0 auto;
+            border-bottom: 1px solid;
+            border-bottom-color: rgba(0, 0, 0, 0.15);
+          }
+
+          :global(.logo) {
+            display: block;
+            position: absolute;
+            top: 36px;
+            z-index: 1;
+            letter0-spacing: initial;
+            opacity: 0.8;
+
+            @below tablet {
+              position: initial;
+            }
+          }
+
+          :global(.logo-symbol) {
+            stroke: black;
+            margin-right: 15px;
+            vertical-align: middle;
+            position: relative;
+            top: -4px;
+          }
+
+          h1 {
+            font-size: 18px;
+            display: inline-block;
+            color: black;
+          }
+
+          :global(.cursor) {
+            margin-left: 3px;
+            opacity: 0.5;
+            filter: invert(100%);
+          }
+
+          &.homepage {
+            .header-content {
+              border-bottom-color: rgba(255, 255, 255, 0.33);
             }
 
             &:after {
-              content: "";
-              position: absolute;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 150px;
-              background: linear-gradient(to bottom, #fff 0, rgba(255, 255, 255, 0) 100%);
-              filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ffffff', GradientType=0 );
-
-              @below tablet {
-                height: 250px;
-              }
-            }
-
-            .header-content {
-              z-index: 1;
-              position: relative;
-              max-width: 979px;
-              margin: 0 auto;
-              padding-bottom: 5px;
-              border-bottom: 1px solid;
-              border-bottom-color: rgba(0, 0, 0, 0.15);
-            }
-
-            :global(.logo) {
-              display: block;
-              position: absolute;
-              letter0-spacing: initial;
-              opacity: 0.8;
-
-              @below tablet {
-                position: initial;
-              }
+              display: none;
             }
 
             :global(.logo-symbol) {
-              stroke: black;
-              margin-right: 15px;
-              vertical-align: middle;
+              stroke: white;
+              fill: #fff;
             }
 
             h1 {
-              font-size: 18px;
-              display: inline-block;
-              color: black;
+              color: white;
             }
 
             :global(.cursor) {
-              margin-left: 3px;
-              opacity: 0.5;
-            }
-
-            &.homepage {
-              .header-content {
-                border-bottom-color: rgba(255, 255, 255, 0.33);
-              }
-
-              &:after {
-                display: none;
-              }
-
-              :global(.logo-symbol) {
-                stroke: white;
-                fill: #fff;
-              }
-
-              h1 {
-                color: white;
-              }
+              filter: none;
             }
           }
-        `}</style>
-      </Fragment>
-    );
-  }
-}
+        }
+      `}</style>
+    </Fragment>
+  );
+};
 
 Header.propTypes = {
-  pages: PropTypes.array.isRequired,
   path: PropTypes.string.isRequired,
   theme: PropTypes.object.isRequired
 };
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,96 +1,279 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import Item from "./Item";
+import { Link } from "gatsby";
+import Search from "../../images/svg/search.svg";
 
-class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const pages = props.pages.map(page => {
-      return {
-        to: page.node.fields.slug,
-        label: page.node.frontmatter.menuTitle
-          ? page.node.frontmatter.menuTitle
-          : page.node.frontmatter.title
-      };
-    });
-
-    this.items = [
-      { to: "/category/", label: "Categories" },
-      { to: "/search/", label: "Search" },
-      ...pages
-    ];
-  }
-
-  static propTypes = {
-    path: PropTypes.string.isRequired,
-    fontLoaded: PropTypes.bool.isRequired,
-    pages: PropTypes.array.isRequired,
-    theme: PropTypes.object.isRequired
-  };
-
-  render() {
-    const { theme } = this.props;
-
-    return (
-      <React.Fragment>
-        <nav className="menu" rel="js-menu">
-          <ul className="itemList">
-            {this.items.map(item => (
-              <Item item={item} key={item.label} theme={theme} />
-            ))}
+const Menu = props => {
+  return (
+    <Fragment>
+      <ul className="main-menu">
+        <li>
+          <Link to="/">Blog</Link>
+          <ul>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Link to="/categories">Popular posts</Link>
+            </li>
           </ul>
-        </nav>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+          <ul>
+            <li>
+              <Link to="/cv">Curriculum</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/geek-life">Geek life</Link>
+            </li>
+            <li>
+              <Link to="/samurai-route">Samurai route</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/work">Work</Link>
+        </li>
 
-        {/* --- STYLES --- */}
-        <style jsx>{`
-          .menu {
-            align-items: center;
-            display: flex;
-            flex-grow: 1;
-            left: 0;
-            padding: 0 ${theme.space.inset.s};
-            width: 100%;
-            transition: all ${theme.time.duration.default};
+        <li className="right">
+          <Link className="btn" to="/contact">
+            Contact
+          </Link>
+          <Link to="/search/" className="search-button">
+            <Search className="search-icon" />
+          </Link>
+        </li>
+      </ul>
+
+      <style jsx>{`
+        .main-menu {
+          position: relative;
+          margin: 0;
+          width: 100%;
+          height: 79px;
+          list-style-type: none;
+          padding-left: 0;
+
+          @below tablet {
+            height: auto;
           }
 
-          .itemList {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            list-style: none;
-            margin: 0;
-            padding: 0; /* 0 ${theme.space.s}; */
+          li {
+            display: inline-block;
             position: relative;
-            width: 100%;
-          }
 
-          @below desktop {
-            .menu {
-              position: initial;
+            @below tablet {
+              margin-bottom: 15px;
             }
-          }
 
-          @from-width desktop {
-            .menu {
-              border-top: none;
-              background: transparent;
-              display: flex;
+            &.right {
+              @above tablet {
+                position: absolute;
+                right: 0;
+                top: 0;
+              }
+            }
+
+            :global(a) {
+              display: inline-block;
               position: relative;
-              justify-content: flex-end;
-              padding-left: 50px;
-              transition: none;
+              color: black;
+              padding: 48px 0 0;
+              font-size: 12px;
+              font-weight: normal;
+              height: 79px;
+              cursor: pointer;
+              text-decoration: none;
+              text-transform: uppercase;
+              border-width: 0 14px;
+              border-color: transparent;
+              border-style: solid;
+              @include box();
+
+              @below tablet {
+                height: auto;
+                padding: 0;
+              }
+
+              @to-width tablet {
+                border-width: 0 5px;
+              }
+
+              &.active,
+              &:hover {
+                color: black;
+
+                &:after {
+                  display: block;
+                  position: absolute;
+                  content: "";
+                  left: 0;
+                  top: 79px;
+                  width: 100%;
+                  height: 1px;
+                  background: black;
+
+                  @to-width tablet {
+                    top: 41px;
+                  }
+
+                  @to-width tablet {
+                    display: none;
+                  }
+                }
+              }
             }
 
-            .itemList {
-              justify-content: flex-end;
+            :global(.btn) {
+              border: 1px solid rgba(0, 0, 0, 0.33);
+              padding: 3px 6px 0;
+              height: 24px;
+              border-radius: 4px;
+              cursor: pointer;
+              white-space: nowrap;
+              transition: background-color ease 0.3s;
+              margin-right: 5px;
+              margin-top: 42px;
+
+              @to-width tablet {
+                margin-top: 0;
+              }
+
+              @below tablet {
+                margin-right: 5px;
+              }
+
+              &:hover {
+                border-color: rgba(0, 0, 0, 0.67);
+                background-color: rgba(0, 0, 0, 0.1);
+              }
+
+              &:hover:after {
+                display: none;
+              }
+
+              &.active:after {
+                display: none;
+              }
+            }
+
+            :global(.search-button) {
+              margin-left: 7px;
               padding: 0;
+              height: initial;
+              font-size: 16px;
+              border-width: 0;
+
+              &:hover:after {
+                display: none;
+              }
+            }
+
+            :global(.search-icon) {
+              fill: black;
+              opacity: 0.8;
+              vertical-align: middle;
+              width: 1em;
+              height: 1em;
+
+              &:hover {
+                opacity: 1;
+                cursor: pointer;
+              }
+            }
+
+            ul {
+              display: none;
+              position: absolute;
+              width: 200px;
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              font-size: 14px;
+              line-height: 20px;
+              z-index: 10;
+              text-align: left;
+              background: white;
+              border: 1px solid rgba(0, 0, 0, 0.15);
+              background-clip: padding-box;
+              border-radius: 5px;
+              box-shadow: 0, 4px, 9px, black;
+
+              li {
+                border-top: 1px solid #eee;
+
+                &:first-child {
+                  border-top: none;
+                }
+
+                :global(a) {
+                  display: block;
+                  position: relative;
+                  margin: 0;
+                  padding: 11px 18px 0;
+                  width: 200px;
+                  height: 42px;
+                  color: black;
+                  background-color: transparent;
+                  border: none;
+                  font-size: 12px;
+                  -webkit-font-smoothing: antialiased;
+                  text-transform: uppercase;
+                  outline: none;
+
+                  &.active,
+                  &:hover {
+                    color: black !important;
+                    background-color: rgba(0, 0, 0, 0.1);
+                  }
+
+                  &:after {
+                    display: none;
+                  }
+                }
+              }
+
+              &:after {
+                display: block;
+                position: absolute;
+                content: "";
+                top: -8px;
+                left: 0;
+                width: 100%;
+                height: 9px;
+                background: transparent;
+
+                @to-width tablet {
+                  top: -38px;
+                  height: 39px;
+                }
+              }
+            }
+
+            &:hover {
+              ul {
+                display: block;
+                top: 85px;
+                left: -4px;
+
+                @to-width tablet {
+                  top: 55px;
+                }
+              }
             }
           }
-        `}</style>
-      </React.Fragment>
-    );
-  }
-}
+        }
+      `}</style>
+    </Fragment>
+  );
+};
+
+Menu.propTypes = {
+  theme: PropTypes.object.isRequired
+};
 
 export default Menu;
